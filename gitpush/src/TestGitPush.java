dd3e71bd648c70b4f18fdb68a82dd42cd2599f49
NoName5990 <61353011+NoName5990@users.noreply.github.com>
2021-04-08 16:29:04 +0800

    Update TestGitPush.java
    这是从远程更新到本地的测试

--- gitpush/src/TestGitPush.java
+++ gitpush/src/TestGitPush.java
@@ -1,6 +1,7 @@
 public class TestGitPush {
     public static void main(String[] args) {
         System.out.println("这是测试idea推送项目到github的测试");
-        System.out.println("加的这一句是为了测试更新的");
+        System.out.println("加的这一句是为了测试更新的！");
+        System.out.println("这是从远程更新到本地的测试！");
     }
 }
