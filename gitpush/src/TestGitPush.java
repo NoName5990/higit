90b37743c3ef56c1c806ed7c67485c6b96170a70
NoName5990 <178219471@qq.com>
2021-04-08 16:26:46 +0800

    这是对文件更新后的提交测试

--- gitpush/src/TestGitPush.java
+++ gitpush/src/TestGitPush.java
@@ -1,6 +1,6 @@
 public class TestGitPush {
     public static void main(String[] args) {
         System.out.println("这是测试idea推送项目到github的测试");
-        System.out.println("加的这一句是为了测试更新的！");
+        System.out.println("加的这一句是为了测试更新的");
     }
 }
