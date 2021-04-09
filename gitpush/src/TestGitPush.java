fb6f2c8ce4a37a07ebc11959c057ac571ec530d8
NoName5990 <178219471@qq.com>
2021-04-09 15:35:29 +0800

    这是测试分支回退和合并的

--- gitpush/src/TestGitPush.java
+++ gitpush/src/TestGitPush.java
@@ -1,5 +1,6 @@
 public class TestGitPush {
     public static void main(String[] args) {
         System.out.println("这是测试idea推送项目到github的测试");
+        System.out.println("必须是每次提交了才能显示记录，提交到本地，才能推送到远程！");
     }
 }
