934d21b3461f2385a0831ac2d58f5647469f6845
NoName5990 <178219471@qq.com>
2021-04-09 16:02:40 +0800

    这是两次提交后合并后的再次提交

--- gitpush/src/TestGitPush.java
+++ gitpush/src/TestGitPush.java
@@ -5,5 +5,6 @@ public class TestGitPush {
         System.out.println("这是从远程更新到本地的测试！");
         System.out.println("测试主分支提交以后，两个分支共同存在的路线!");
         System.out.println("必须是每次提交了才能显示记录，提交到本地，才能推送到远程！");
+        System.out.println("提交信息");
     }
 }
