5cef35de3304c423df90546eb4f377fe52117455
NoName5990 <178219471@qq.com>
2021-04-09 20:05:56 +0800

    本地合并远程分支

--- gitpush/src/TestGitPush.java
+++ gitpush/src/TestGitPush.java
@@ -2,5 +2,6 @@ public class TestGitPush {
     public static void main(String[] args) {
         System.out.println("这是测试idea推送项目到github的测试");
         System.out.println("必须是每次提交了才能显示记录，提交到本地，才能推送到远程！");
+        System.out.println("从本地控制远程仓库，这是自己的，非默认的！");
     }
 }
