95598c87de1c8b28bd9f74f1e3d029be315c89be
NoName5990 <178219471@qq.com>
2021-04-09 15:39:46 +0800

    测试主分支和从分支路线

--- gitpush/src/TestGitPush.java
+++ gitpush/src/TestGitPush.java
@@ -3,5 +3,6 @@ public class TestGitPush {
         System.out.println("这是测试idea推送项目到github的测试");
         System.out.println("加的这一句是为了测试更新的！");
         System.out.println("这是从远程更新到本地的测试！");
+        System.out.println("测试主分支提交以后，两个分支共同存在的路线!");
     }
 }
